e0774dfb14738d291df60228c6fc68e7b0c62b94
Tyler Bennett <tbennett6421@gmail.com>
2017-08-12 20:35:40 -0500

    Initial Modfication

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-set :user, "albert"
+set :user, "danbooru"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "danbooru.nsecure.me", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "danbooru.nsecure.me", :roles => %w(web app), :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
