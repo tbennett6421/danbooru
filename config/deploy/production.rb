d1434719021acc8507babab3f0ea3a2592621c49
Tyler Bennett <tbennett6421@gmail.com>
2017-08-12 21:49:10 -0500

    linked file error

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :rails_env, "production"
 server "127.0.0.1", :roles => %w(web app db), :primary => true, :user => "danbooru"
 server "127.0.0.1", :roles => %w(web app), :user => "danbooru"
 
-set :linked_files, fetch(:linked_files, []).push(".env.production")
+#set :linked_files, fetch(:linked_files, []).push(".env.production")
