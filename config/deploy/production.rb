bae6236f1359964432ccdd8aabc4cf96a69c9b10
Tyler Bennett <tbennett6421@gmail.com>
2017-08-12 21:44:26 -0500

    Change Server du to fatal error during deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "danbooru.nsecure.me", :roles => %w(web app db), :primary => true, :user => "danbooru"
-server "danbooru.nsecure.me", :roles => %w(web app), :user => "danbooru"
+server "127.0.0.1", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "127.0.0.1", :roles => %w(web app), :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
